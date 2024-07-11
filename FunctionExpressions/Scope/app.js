d2c3656e2a41fc865703c04a909e92509107ca28
Aditya Singh <asks100904@gmail.com>
2024-07-11 20:34:10 +0530

    learnt more about destructuring objects and destructuring params

--- FunctionExpressions/Scope/app.js
+++ FunctionExpressions/Scope/app.js
@@ -1,34 +1,34 @@
-// let totalEggs = 0;
-// function collectEggs() {
-//     totalEggs = 6;
-// }
+let totalEggs = 0;
+function collectEggs() {
+    totalEggs = 6;
+}
 
-// console.log(totalEggs);
-// collectEggs();
-// console.log(totalEggs);
+console.log(totalEggs);
+collectEggs();
+console.log(totalEggs);
 
-// let bird = 'Scarlet Macaw';
-// function birdWatch() {
-//     // let bird = 'Great Blue Heron';
-//     console.log(bird);
-// }
-// birdWatch()
+let bird = 'Scarlet Macaw';
+function birdWatch() {
+    // let bird = 'Great Blue Heron';
+    console.log(bird);
+}
+birdWatch()
 
-// let radius = 8;
-// if (radius > 0) {
-//     const PI = 3.14159;
-//     let msg = 'HIII';
-// }
+let radius = 8;
+if (radius > 0) {
+    const PI = 3.14159;
+    let msg = 'HIII';
+}
 
-// console.log(radius);
-// console.log(msg)
+console.log(radius);
+console.log(msg)
 
-// for (let i = 0; i < 5; i++) {
-//     var msg = 'ASDFSD';
-//     console.log(msg);
-// }
-// console.log(msg);
-// console.log(i);
+for (let i = 0; i < 5; i++) {
+    var msg = 'ASDFSD';
+    console.log(msg);
+}
+console.log(msg);
+console.log(i);
 
 function bankRobbery() {
     const heroes = ['Spiderman', 'Wolverine', 'Black Panther'];
--- Newer JS Features/Destructuring/app.js
+++ Newer JS Features/Destructuring/app.js
@@ -4,3 +4,44 @@ const lowScore = scores[0];
 const secondlowScore = scores[1];
 
 const [gold, silver, bronze, ...everyoneElse] = scores;
+
+const user = {
+    email: 'harvey@gmail.com',
+    password: 'sCsof8ehks',
+    firstName: 'Harvey',
+    lastName: 'Milk',
+    born: 1930,
+    died: 1980,
+    city: 'San Francisco',
+    state: 'California'
+}
+
+const user2 = {
+    email: 'Stacy@gmail.com',
+    firstName: 'Stacy',
+    lastName: 'Gonzales',
+    born: 1987,
+    city: 'Tulsa',
+    state: 'Oklahoma'
+}
+
+// const firstName = user.firstName;
+// const lastName = user.lastName;
+
+// const email = user.email;
+// const { email, firstName, lastName, city } = user;
+
+// const { born: birthYear, died: deathYear = 'N/A', bornPlace = 'N/A'} = user2;
+
+// function fullName(user) {
+//     return `${user.firstName} ${user.lastName}`;
+// }
+
+// function fullName(user) {
+//     const { firstName, lastName } = user;
+//     return `${firstName} ${lastName}`;
+// }
+
+function fullName({ firstName, lastName}) {
+    return `${firstName} ${lastName}`;
+}
