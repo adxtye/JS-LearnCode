5d2c374428fcb227deb4b6b681208b987c3422ce
Aditya Singh <asks100904@gmail.com>
2024-09-11 17:43:29 +0530

    Learned DOM Selecting

--- DOM_Intro_CODE/Selecting/app.js
+++ DOM_Intro_CODE/Selecting/app.js
@@ -14,10 +14,10 @@
 //     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
 // }
 
-const links = document.querySelectorAll('p a');
+// const links = document.querySelectorAll('p a');
 
-for(let link of links) {
-    console.log(link.href);
-}
+// for(let link of links) {
+//     console.log(link.href);
+// }
 
 
